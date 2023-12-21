179f2a0f33055141f6e581cf35ca5668e89c00b3
Pavel <Pavel.Vlasov@nasdanika.org>
2023-12-20 19:57:35 -0500

    SVG by default

--- src/main/java/org/nasdanika/diagramgenerator/plantuml/PlantUMLDiagramGenerator.java
+++ src/main/java/org/nasdanika/diagramgenerator/plantuml/PlantUMLDiagramGenerator.java
@@ -12,6 +12,20 @@ import net.sourceforge.plantuml.SourceStringReader;
 
 public class PlantUMLDiagramGenerator implements DiagramGenerator {
 	
+	private boolean png;
+	
+	public PlantUMLDiagramGenerator() {
+		this(false);
+	}
+
+	/**
+	 * 
+	 * @param png Renders PNG if true, SVG otherwise.
+	 */
+	public PlantUMLDiagramGenerator(boolean png) {
+		this.png = png;
+	}
+	
 	@Override
 	public boolean isSupported(String dialect) {
 		return DiagramGenerator.UML_DIALECT.equals(dialect)
@@ -36,45 +50,49 @@ public class PlantUMLDiagramGenerator implements DiagramGenerator {
 			
 			SourceStringReader reader = new SourceStringReader(sb.toString());
 			
-			FileFormatOption fileFormatOption = new FileFormatOption(FileFormat.PNG);
+			FileFormatOption fileFormatOption = new FileFormatOption(png ? FileFormat.PNG : FileFormat.SVG);
 			reader.outputImage(baos, 0, fileFormatOption);		
-			String diagramCMap = reader.getCMapData(0, fileFormatOption);
 			baos.close();
-
-			StringBuilder ret = new StringBuilder("<img src=\"data:image/png;base64, ");
-			ret
-				.append(Base64.getEncoder().encodeToString(baos.toByteArray()))
-				.append("\"");
 			
-			if (org.nasdanika.common.Util.isBlank(diagramCMap)) {
-				ret.append("/>");
+			if (png) {
+				String diagramCMap = reader.getCMapData(0, fileFormatOption);
+	
+				StringBuilder ret = new StringBuilder("<img src=\"data:image/png;base64, ");
+				ret
+					.append(Base64.getEncoder().encodeToString(baos.toByteArray()))
+					.append("\"");
+				
+				if (org.nasdanika.common.Util.isBlank(diagramCMap)) {
+					ret.append("/>");
+					return ret.toString();			
+				}
+				
+				String openingTag = "<map id=\"plantuml_map\" name=\"plantuml_map\">";
+				if (diagramCMap.startsWith(openingTag)) {
+					String mapId = "plantuml_map_" + UUID.randomUUID().toString();
+					ret			
+					.append(" usemap=\"#")
+					.append(mapId)
+					.append("\"/>")
+					.append(System.lineSeparator())
+					.append("<map id=\"")
+					.append(mapId)
+					.append("\" name=\"")
+					.append(mapId)
+					.append("\">")
+					.append(diagramCMap.substring(openingTag.length()));
+					
+				} else {				
+					ret			
+						.append(" usemap=\"#plant_uml_map\"/>")
+						.append(System.lineSeparator())
+						.append(diagramCMap);
+					return ret.toString();
+				}
 				return ret.toString();			
 			}
-			
-			String openingTag = "<map id=\"plantuml_map\" name=\"plantuml_map\">";
-			if (diagramCMap.startsWith(openingTag)) {
-				String mapId = "plantuml_map_" + UUID.randomUUID().toString();
-				ret			
-				.append(" usemap=\"#")
-				.append(mapId)
-				.append("\"/>")
-				.append(System.lineSeparator())
-				.append("<map id=\"")
-				.append(mapId)
-				.append("\" name=\"")
-				.append(mapId)
-				.append("\">")
-				.append(diagramCMap.substring(openingTag.length()));
-				
-			} else {				
-				ret			
-					.append(" usemap=\"#plant_uml_map\"/>")
-					.append(System.lineSeparator())
-					.append(diagramCMap);
-				return ret.toString();
-			}
-									
-			return ret.toString();			
+
+			return new String(baos.toByteArray());
 		} catch (Exception e) {
 			return "<div class=\"nsd-error\">Error during diagram rendering: " + e + "</div>";
 		}
